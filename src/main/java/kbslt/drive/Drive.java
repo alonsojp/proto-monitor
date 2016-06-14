a2a6a59327d2b4e36c6a9eca947d567b1388c54d
Jean-Pierre <dev.alonsojp@gmail.com>
2016-06-14 12:25:42 +0200

    requested connections is now bound to the slider

--- src/main/java/kbslt/drive/Drive.java
+++ src/main/java/kbslt/drive/Drive.java
@@ -9,7 +9,7 @@ public interface Drive {
     void start(boolean mode_console);
     int getRequestedConnections();
     IntegerProperty requestedConnectionsProperty();
-    void setRequestedConnections(int requestedConnections);
+//    void setRequestedConnections(int requestedConnections);
     int getStartedConnections();
     IntegerProperty startedConnectionsProperty();
     int getConnectedConnections();
